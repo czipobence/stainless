5da2e430fcbb6a207bcf66b513a515d16e2fbbb7
jad <jad.hamza@epfl.ch>
2017-06-29 22:58:31 +0200

    working simplifier

--- core/src/main/scala/stainless/transformers/ProgramSimplifier.scala
+++ core/src/main/scala/stainless/transformers/ProgramSimplifier.scala
@@ -44,9 +44,9 @@ trait ProgramSimplifier { self =>
   })
 
 
-  private def removeAssertsAndRequires(syms: Symbols): Symbols = {
+  private def removeAssertsAndRequires(funs: Map[Identifier, FunDef]): Map[Identifier, FunDef] = {
 
-    val newFunctions = syms.functions.values.map { fd =>
+    funs.mapValues { fd =>
       val newBody = exprOps.preMap({
         case Assert(_, _, body) => Some(body)
         case Require(_, body) => Some(body)
@@ -54,13 +54,14 @@ trait ProgramSimplifier { self =>
       }, applyRec = true)(fd.fullBody)
 
       fd.copy(fullBody = newBody)
-    }.toSeq
-
-    NoSymbols.withFunctions(newFunctions).withADTs(syms.adts.values.toSeq)
+    }
   }
 
   private def removeIndex[T](s: Seq[T], i: Int): Seq[T] =
     s.zipWithIndex.filter{ case (t,j) => j != i }.map(_._1)
+
+  private def removeIndices[T](init: Seq[T], is: List[Int]): Seq[T] =
+    is.foldLeft(init) { case (s, i) => removeIndex(s,i) }
 
   private def removeParameter(e: Expr, id: Identifier, parameterToRemove: Int): Expr = {
     import trees._
@@ -76,8 +77,26 @@ trait ProgramSimplifier { self =>
     }(e)
   }
 
+  private def removeParameters(e: Expr, parametersToRemove: Map[Identifier, List[Int]]): Expr = {
+    import trees._
+
+    exprOps.postMap {
+      case fi@FunctionInvocation(id, tps, args) =>
+        Some(FunctionInvocation(
+          id,
+          tps,
+          removeIndices(args, parametersToRemove.getOrElse(id, Nil))).copiedFrom(fi))
+      case _ =>
+        None
+    }(e)
+  }
+
   private def removeParameter(vc: VC, id: Identifier, parameterToRemove: Int): VC = {
     verification.VC(removeParameter(vc.condition, id, parameterToRemove), vc.fd, vc.kind)
+  }
+
+  private def removeParameters(vc: VC, parametersToRemove: Map[Identifier, List[Int]]): VC = {
+    verification.VC(removeParameters(vc.condition, parametersToRemove), vc.fd, vc.kind)
   }
 
   private def removeParameter(fd: FunDef, id: Identifier, parameterToRemove: Int): FunDef = {
@@ -89,45 +108,105 @@ trait ProgramSimplifier { self =>
     println("simplifying program")
 //    println(syms)
 
-    val noassertSymbs = removeAssertsAndRequires(syms)
+    val noassertFuns = removeAssertsAndRequires(syms.functions)
     val noassertVCs = vcs.map(transformVC(removeAsserts))
 
     println("======================")
     println("removed asserts")
 //    println(noassertSymbs)
 
-    def loop(syms: Symbols, vcs: Seq[VC]): (Symbols, Seq[VC]) = {
-      println("======================")
-      println("looping")
-//      println(syms)
-      for ((id, fd) <- syms.functions if (!fd.flags.exists(flag => flag match {
-        case Extern => true
-        case Annotation("library", _) => true
-        case _ => false
-      }))) {
-        val freeVariables = exprOps.variablesOf(fd.fullBody).map(v => v.id)
-        fd.params.zipWithIndex.find { case (vd,_) => !freeVariables.contains(vd.id) } match {
-          case Some((vd,i)) =>
-            println(fd.flags)
-            println("Removing %s at index %s from:".format(vd,i))
-            println(fd)
-            val calls = inox.Bench.time("getting callers", callers(fd))
-            inox.Bench.reportS()
-            val funs2 = calls.foldLeft(syms.functions) {
-              case (funDefs, fd2) =>
-                funDefs.updated(fd2.id, removeUnusedLets(removeParameter(fd2, id, i)))
-            }
-            val funs3 = funs2.updated(id, funs2(id).copy(params = removeIndex(funs2(id).params, i)))
-            val newVCs = vcs.map((vc: VC) => removeUnusedLets(removeParameter(vc, id, i)))
-            val newSyms = NoSymbols.withFunctions(funs3.values.toSeq).withADTs(syms.adts.values.toSeq)
-            loop(newSyms, newVCs)
-          case None => ()
+    def loop( funs: Map[Identifier, FunDef], 
+              toVisit: Set[Identifier], 
+              modifiers: Map[Identifier, List[Int]])
+              : (Map[Identifier, FunDef], Map[Identifier, List[Int]]) = {
+      println("to visit: " + toVisit.size)
+      if (toVisit.isEmpty) (funs, modifiers)
+      else {
+
+        val id = toVisit.head
+        val fd = funs(id)
+        if (fd.flags.exists(flag => flag match {
+          case Extern => true
+          case Annotation("library", _) => true
+          case _ => false
+        })) loop(funs, toVisit.tail, modifiers)
+        else {
+          val freeVariables = exprOps.variablesOf(fd.fullBody).map(v => v.id)
+          fd.params.zipWithIndex.find { case (vd,_) => !freeVariables.contains(vd.id) } match {
+            case Some((vd,i)) =>
+              val calls = inox.Bench.time("getting callers", callers(fd).map(_.id))
+              // println(fd.flags)
+              println("Removing %s at index %s from %s".format(vd,i,id.name))
+              // println("The callers are")
+              // println(calls.map(id => id.name))
+              // println(fd)
+
+
+              // inox.Bench.reportS()
+              val funs2 = calls.foldLeft(funs) { case (funDefs, id2) =>
+                val fd2 = funs(id2)
+                // println("removing parameter (%s,%s) from: %s".format(id.name,i,fd2.id.name))
+                val newFD2 = removeUnusedLets(removeParameter(fd2, id, i))
+                // println(newFD2)
+                funDefs.updated(fd2.id, newFD2)
+              }
+              val funs3 = funs2.updated(id, funs2(id).copy(
+                params = removeIndex(funs2(id).params, i)
+              ))
+              val newModifiers = modifiers.updated(id, modifiers.getOrElse(id, Nil) :+ i)
+              
+              // println("new type!")
+              val newFD = funs3(id)
+              val tempSymbols = NoSymbols.withFunctions(funs3.values.toSeq).withADTs(syms.adts.values.toSeq)
+              // println("===================")
+              // println("===================")
+              // println("===================")
+              // println("===================")
+              // println(tempSymbols)
+              // println("===================")
+              // println("===================")
+              // println("===================")
+              // println("===================")
+              for (fdTemp <- funs3.values) {
+                try {
+                  tempSymbols.typeCheck(fdTemp.fullBody, fdTemp.returnType)
+                } catch {
+                  case e: Throwable =>
+                    println("EXPLAINING: " + fdTemp.id.name)
+                    println(tempSymbols.explainTyping(fdTemp.fullBody))
+                    throw new Exception("YABLOL")
+                }
+                
+                // try {
+                //   tempSymbols.typeCheck(fdTemp.fullBody, fdTemp.returnType)
+                // } catch {
+                //   case e: Throwable => 
+                //     tempSymbols.explainTyping(fdTemp.fullBody)
+                // }
+              }
+
+              loop(funs3, toVisit.tail ++ calls, newModifiers)
+            case None =>
+              loop(funs, toVisit.tail, modifiers)
+         }
         }
       }
-      (syms, vcs)
     }
 
-    loop(noassertSymbs, noassertVCs)
+    val (funs, modifiers) = loop(noassertFuns, syms.functions.values.map(_.id).toSet, Map())
+    // val (funs, modifiers) = (noassertFuns, Map[Identifier,List[Int]]())
+    val newSyms = NoSymbols.withFunctions(funs.values.toSeq).withADTs(syms.adts.values.toSeq)
+    
+    // for (vc <- noassertVCs) {
+    //   val newVC = removeUnusedLets(removeParameters(vc, modifiers))
+
+    //   println("typechecking")
+    //   println(newVC.condition)
+
+    //   // println(newSyms.typeCheck(newVC.condition))
+    // }
+    
+    (newSyms, noassertVCs.map(vc => removeUnusedLets(removeParameters(vc, modifiers))))
   }
 }
 
